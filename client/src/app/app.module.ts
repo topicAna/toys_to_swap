37cef4ea154d3e12aa83fbf9ef0e2e9065ebff57
AnaTopic <atopic.ie@gmail.com>
2019-12-18 23:32:40 +0100

    landing page animation finished

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './shared/landing-page/landing-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LandingPageComponent,
+    MenuComponent,
+    FooterComponent
+  ],
+
+  imports:[
     BrowserModule,
     AppRoutingModule,
-    LandingPageComponent
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
